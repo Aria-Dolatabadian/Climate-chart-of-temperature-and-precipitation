0927205a6b5fcafbaace8a2e90ac9b578d10b4eb
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-06-02 10:43:58 +0800

    Update Code.R

--- Code.R
+++ Code.R
@@ -35,4 +35,42 @@ plot_TH1(
 
 
 
+#OR
+
+
+library(AgroR)
+
+w <- read.csv("weather.csv")
+head(w)
+
+plot_TH(
+  w$tempo,
+  w$Tmed,
+  w$Tmax,
+  w$Tmin,
+  w$Precipitation,
+  xlab = "Time",
+  yname1 = expression("Precipitation (mm)"),
+  yname2 = expression("Temperature ("^o * "C)"),
+  legend.H = "Precipitation",
+  legend.tmed = "Tmed",
+  legend.tmin = "Tmin",
+  legend.tmax = "Tmax",
+  colormax = "red",
+  colormin = "blue",
+  colormean = "darkgreen",
+  fillbar = "gray80",
+  limitsy1 = c(0, 100),
+  x = "days",
+  breaks = "1 months",
+  textsize = 12,
+  legendsize = 12,
+  titlesize = 12,
+  linesize = 1,
+  date_format = "%m-%Y",
+  sc = 2.5,
+  angle = 0,
+  legend.position = "bottom",
+  theme = theme_classic()
+)
 
